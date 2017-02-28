4e3065ce30850bd47d816d91203c9ec7be664858
ceociocto <ceociocto@qq.com>
2017-02-28 12:50:56 +0800

    added router

--- src/app/main.js
+++ src/app/main.js
@@ -3,9 +3,34 @@ import VueRouter from 'vue-router';
 // don't worry, we haven't created this yet!
 import AppComponent from './components/app-component/app-component';
 
+Vue.use(VueRouter)
+
+const Foo = {template: '<div>foo</div>'}
+
+const Bar = {template: '<div>bar</div>'}
+
+const com = AppComponent
+
+const routes = [
+	{path: '/foo', component: Foo},
+	{path: '/bar', component: Bar},
+	{path: '/twitter', component: com}
+]
+
+const router = new VueRouter({
+	routes
+})
+
+
+const app = new Vue({
+	el: '#app',
+  router
+})
+
+/*
 new Vue({
   el: '#app',
   components: {
     'app-component': AppComponent
   }
-});
+}).use(VueRouter);*/
